bc778445b9e29cab65f504c04e574692513ff76c
Ernesto Freyre <ernestofreyreg@gmail.com>
2024-03-02 00:18:17 +0000

    initial commit

---
+++ src/components/Jumbotron.jsx
@@ -0,0 +1,18 @@
+import { Button, Col, Row } from "react-bootstrap";
+
+export function Jumbotron() {
+  return (
+    <Row>
+      <Col>
+        <h1>Here</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam
+          tenetur magni veniam eos provident commodi dolore molestias, molestiae
+          nemo ab rem? Numquam, totam accusamus! Fugiat aspernatur id voluptas
+          omnis facere!
+        </p>
+        <Button variant="primary">CTA</Button>
+      </Col>
+    </Row>
+  );
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,10 @@
+import { Jumbotron } from "@/components/Jumbotron";
+import { Container } from "react-bootstrap";
+
+export default function Home() {
+  return (
+    <Container>
+      <Jumbotron />
+    </Container>
+  );
+}
