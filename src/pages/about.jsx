7a4ef4bf1f482afcb8a0c7050dd65538827c076d
Ernesto Freyre <ernestofreyreg@gmail.com>
2024-03-02 00:45:34 +0000

    changed name

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,7 +4,7 @@ import { Container } from "react-bootstrap";
 export default function About() {
   return (
     <Container>
-      <p>Template created by E. Freyre @efreyreg</p>
+      <p>Template created by Ernesto Freyre @efreyreg</p>
       <Link href="/">Home</Link>
     </Container>
   );
