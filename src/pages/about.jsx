e647afe4136a8b24aff06e1cda9f7997d45ee88d
Ernesto Freyre <ernestofreyreg@gmail.com>
2024-03-02 00:29:51 +0000

    changes

---
+++ src/pages/about.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import { Container } from "react-bootstrap";
+
+export default function About() {
+  return (
+    <Container>
+      <p>Template created by E. Freyre @efreyreg</p>
+      <Link href="/">Home</Link>
+    </Container>
+  );
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,12 @@
 import { Jumbotron } from "@/components/Jumbotron";
+import Link from "next/link";
 import { Container } from "react-bootstrap";
 
 export default function Home() {
   return (
     <Container>
       <Jumbotron />
+      <Link href="/about">About</Link>
     </Container>
   );
 }
